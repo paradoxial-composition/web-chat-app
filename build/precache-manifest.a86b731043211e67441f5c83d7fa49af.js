9c7104d428516a7730d5b630470d86ff3a0a076d
paradoxial-composition <elbani.othman@gmail.com>
2021-10-07 12:18:13 +0100

    some style modification and build

---
+++ build/precache-manifest.a86b731043211e67441f5c83d7fa49af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1e9d832134221a1c0204e901d3dad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63908e6dd5c7e421e0b2",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "26b3969a077b527939af",
+    "url": "/static/css/main.b1fa7a8c.chunk.css"
+  },
+  {
+    "revision": "63908e6dd5c7e421e0b2",
+    "url": "/static/js/2.84cf1242.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.84cf1242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b3969a077b527939af",
+    "url": "/static/js/main.32ada1df.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
